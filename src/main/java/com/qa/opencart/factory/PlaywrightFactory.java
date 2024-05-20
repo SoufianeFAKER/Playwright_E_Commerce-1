e7ecdf52da9c4a92d8251eac94132e0cc675aa41
SoufianeFAKER <soufian.faker@gmail.com>
2024-05-20 21:01:36 +0100

    First Commit

---
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -0,0 +1,85 @@
+package com.qa.opencart.factory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Page;
+
+public class PlaywrightFactory {
+	
+	Playwright playwright;
+	Browser browser;
+	BrowserContext browserContext;
+	Page page;
+	
+	Properties prop;
+	
+	public Page initBrowser(Properties prop) {
+		
+		String browserName = prop.getProperty("browser").trim();
+		
+		System.out.println("browser name is: " + browserName);
+		
+		playwright = Playwright.create();
+		
+		// Cross Browser
+		
+		switch (browserName.toLowerCase()) {
+		case "chromium":
+			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			break;
+		case "firefox":
+			browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			break;
+		case "safari":
+			browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			break;
+		case "chrome":
+			browser = playwright.chromium().launch(new LaunchOptions().setChannel("chrome").setHeadless(false));
+			break;
+
+		default:
+			System.out.println("Please pass the right browser name....");
+			break;
+		}
+		
+		browserContext = browser.newContext();
+		page = browserContext.newPage();
+		page.navigate(prop.getProperty("url").trim());
+		
+		return page;
+	}
+	
+	/**
+	 * this method is used to initialize the properties from config file
+	 * @return 
+	 */
+	public Properties init_prop() {
+		
+		try {
+			
+			FileInputStream ip = new FileInputStream("src/test/resources/config/config.properties");
+			prop = new Properties();
+			prop.load(ip);
+		
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		return prop;
+	}
+
+}
